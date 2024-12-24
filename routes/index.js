159b98977c8e72808a8e96d190e14927bef504dd
seedbox-andi <andimagsumbol@gmail.com>
2024-12-24 14:40:52 +0800

    created routes for index and db test connections

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Sequelize } = require('sequelize');
+const config = require('../config/config.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    try {
+        res.json({
+            status: 1,
+            message: `Welcome to POS & Inventory System by andimags.`
+        })
+    }
+    catch (err) {
+        res.json({
+            status: 0,
+            message: `Something went wrong: ${err.message}`
+        })
+    }
+});
+
+router.get('/health-check', async function (req, res, next) {
+    try {
+        const env = process.env.ENVIRONMENT;
+        const dbConfig = config[env];
+        
+        const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+            host: 'localhost',
+            dialect: 'mysql'
+          });
+
+        await sequelize.authenticate();
+
+        res.json({
+            status: 1,
+            message: `Connection has been established successfully.`
+        })
+    }
+    catch (err) {
+        res.json({
+            status: 0,
+            message: `Something went wrong: ${err.message}`
+        })
+    }
 });
 
 module.exports = router;
