53366c8587f315adab63f14cb8164f7514a693fb
andimags <andimagsumbol@gmail.com>
2025-01-07 13:43:08 +0800

    added error msg on auth middleware if the token is not found

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,6 +3,14 @@ const jwt = require('jsonwebtoken');
 function authMiddleware(req, res, next) {
     try {
         const { authorization } = req.headers;
+
+        if(!authorization){
+            return res.json({
+                status: 0,
+                message: "Token not found."
+            })
+        }
+
         const token = authorization.split(' ')[1];
 
         jwt.verify(token, process.env.PRIVATE_KEY);
