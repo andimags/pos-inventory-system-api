90875469333c086c9321f8762fc3e0f6925e40d8
andimags <andimagsumbol@gmail.com>
2025-01-17 16:15:53 +0800

    fix: 4th part of barcode being null when using stocks seeder

--- seeders/20250117072651-stocks.js
+++ seeders/20250117072651-stocks.js
@@ -6,45 +6,33 @@ const { faker } = require('@faker-js/faker');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        const stocks = [];
-
         for (let i = 0; i < 10; i++) {
             const price = faker.commerce.price({ min: 20, max: 200 });
             const quantity = faker.number.int({ min: 5, max: 30 });
-
-            // Fetch random Product and Supplier
+    
             const product = await Product.findOne({
-                order: [
-                    Sequelize.fn('RAND'),
-                ]
+                order: [Sequelize.fn('RAND')],
             });
             const supplier = await Supplier.findOne({
-                order: [
-                    Sequelize.fn('RAND'),
-                ]
+                order: [Sequelize.fn('RAND')],
             });
-
+    
             if (product && supplier) {
-                const stock = Stock.build({
+                const stock = await Stock.create({
                     price,
                     quantity,
                     product_id: product.id,
                     supplier_id: supplier.id,
-                });
-
-                stocks.push({
-                    price,
-                    quantity,
-                    product_id: product.id,
-                    supplier_id: supplier.id,
-                    barcode: stock.generateBarcode(), // Use Stock's generateBarcode method
                     created_at: new Date(),
                     updated_at: new Date(),
                 });
+    
+                const barcode = stock.generateBarcode()
+    
+                stock.barcode = barcode;
+                await stock.save();
             }
         }
-
-        await queryInterface.bulkInsert('stocks', stocks, {});
     },
 
     async down(queryInterface, Sequelize) {
